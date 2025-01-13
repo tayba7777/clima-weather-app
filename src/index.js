457b004b08910ff11ff0f71998217198b4d042ce
tayba7777 <tayba.elias.moh@gmail.com>
2025-01-13 12:26:04 +0200

    .

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function refreshWeather(response) {
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
-    humidityElement.innerHTML = `${response.data.temperature.humidity}`;
+    humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
     windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
