1102d30255976fc7741c2c6a0579d41f1c8c0e91
tayba7777 <tayba.elias.moh@gmail.com>
2025-01-16 16:07:19 +0200

    .

--- src/index.js
+++ src/index.js
@@ -59,4 +59,4 @@ function refreshWeather(response) {
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-  searchCity("london");
+  searchCity("Khartoum");
